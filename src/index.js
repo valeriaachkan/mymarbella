204eb1591d575840edcc70f750ffffe5c59ff062
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-03-24 16:58:07 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles/main.scss';
+// import locs from '../locs.json';
+
+// console.log(locs);
+// const locations = locs.location;
+// console.log(locations);
+
+// const locationSelect = locations
+// 	.map(
+// 		(location) =>
+// 			`<label>
+//             ${location}
+//             <input class="checkbox" type="checkbox" value='${location
+// 							.toLowerCase()
+// 							.split(' ')
+// 							.join('-')}' />
+//                 </label>`
+// 	)
+// 	.join('');
+// console.log(locationSelect);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	devtool: 'eval-source-map',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/,
+				include: [__dirname + '/src'],
+				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: 'asset/inline',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true,
+			},
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'src'),
+		},
+		compress: true,
+		port: 9000,
+	},
+};
