a020296d6c8421388a9ac993f44175bdfb86ed39
nkpn <illinmikita@gmail.com>
2023-04-30 16:10:33 +0300

    WORKING BACKEND

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -7,21 +7,21 @@ export default class ResalesOnlineApi {
 
 	constructor() {
 		this.#p1 = '1014905';
-		this.#p2 = '03ca0745e539d0304361b3187c3b36ae598c38a1';
-		this.#baseUrl = 'https://webapi.resales-online.com/V6/SearchProperties';
-		this.headers = {
-			'Access-Control-Allow-Origin': 'https://backend.resales-online.com',
-			'Content-Type': 'application/json',
-		};
-		// this.P_Currency = 'EUR';
+		this.#p2 = 'ca3ca58bbda23243ae3ed9e97098099d7fff44d7';
+		this.#baseUrl = 'https://booking-bff-test.up.railway.app/searchProperties';
+		// this.headers = {
+		// 	'Access-Control-Allow-Origin': 'https://backend.resales-online.com',
+		// 	'Content-Type': 'application/json',
+		// };
+		this.P_Currency = 'EUR';
 		this.p_PageSize = '20'; //Amount of properties returned. Default: 10. Max. value:40
 		this.page = 1; //Page number of the search results to be returned. Default:1
-		// this.p_agency_filterid = '1'; //1-Sale, 2-STRent, 3-LTRent
-		// this.P_Location = ''; //Specific Location or csv list of Locations (e.g. Calahonda, Elviria)
-		// this.P_PropertyTypes = '2-3,2-4'; //List of one or more Option Values
-		// this.P_Max = '';
-		// this.P_Min = '';
-		// this.P_MustHaveFeatures = ''; //If features are selected, affects either the sort order or the search results
+		this.p_agency_filterid = '1'; //1-Sale, 2-STRent, 3-LTRent
+		this.P_Location = ''; //Specific Location or csv list of Locations (e.g. Calahonda, Elviria)
+		this.P_PropertyTypes = '2-3,2-4'; //List of one or more Option Values
+		this.P_Max = '';
+		this.P_Min = '';
+		this.P_MustHaveFeatures = ''; //If features are selected, affects either the sort order or the search results
 		this.searchQueryId = ''; //The unique identifier for this search (returned by the initial query). SearchProperties?P_PageSize=5&P_QueryId=11111111-aaa&P_PageNo=2. Will return the next 5 properties from the previous search
 	}
 
@@ -30,12 +30,12 @@ export default class ResalesOnlineApi {
 			method: 'GET',
 			// redirect: 'follow',
 			// mode: 'no-cors',
-			...this.headers,
+			// ...this.headers,
 		};
 		const searchParams = new URLSearchParams({
 			p1: this.#p1,
 			p2: this.#p2,
-			headers: this.headers,
+			// headers: this.headers,
 			p_sandbox: true,
 			p_PageSize: this.p_PageSize,
 			p_PageNo: this.page,
@@ -45,17 +45,13 @@ export default class ResalesOnlineApi {
 
 		const url = `${this.#baseUrl}?${searchParams}`;
 
-		// const response = await axios.get(url, {
-		// 	mode: 'no-cors',
-		// });
-
-		// console.log(response);
-
 		try {
 			const response = await fetch(url, requestOptions);
+			
+			console.log(response.json());
 			// const data = await response.json();
-			console.log(response);
 			this.incrementPage();
+			// return data;
 		} catch (error) {
 			console.log(error);
 		}
