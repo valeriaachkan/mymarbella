0d162e26e760d7a82c5f5aee9880be82664746e2
nkpn <illinmikita@gmail.com>
2023-05-15 22:48:07 +0300

    form send correct data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import $, { type } from 'jquery';
 import './styles/main.scss';
 import ResalesOnlineApi from './js/api-service';
-import {customFormInit , setQueryParameter} from './js/form.js';
+import {customFormInit } from './js/form.js';
 
 const APIRequest = new ResalesOnlineApi();
 const form = document.querySelector('form');
@@ -12,18 +11,16 @@ customFormInit();
 // custom submit function
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    const query = setQueryParameter();
-
-    APIRequest.fetchProperties(...query, {P_Location:'Alicante'});
+    const formData = {};
+    formData.P_Location = $('[name="P_Location"]').val();
+    formData.p_agency_filterid = $('#p_agency_filterid_2').val();
+     formData.P_PropertyTypes = $('[name="P_PropertyTypes"]').val();
+    formData.P_Min=$('[name="P_Min"]').val();
+    formData.P_Max=$('[name="P_Max"]').val();
+    formData.P_RefId=$('[name="P_RefId"]').val();
+    formData.P_Beds=$('[name="P_Beds"]').val();
+    formData.P_Baths=$('[name="P_Baths"]').val();
+    
+    
+    APIRequest.fetchProperties(formData);
 });
-
-
-
-// const data = APIRequest.fetchProperties({
-// 	p_agency_filterid: '3',
-// 	p_PageSize: '20',
-// 	P_Location: 'Benalmadena',
-// 	P_PropertyTypes: '1-1',
-// 	P_Min: '6000',
-// });
--- src/js/form.js
+++ src/js/form.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
-
+import 'bootstrap';
+import 'bootstrap-select';
 const form = document.querySelector('form');
 
 export function customFormInit() {
-	const form = document.querySelector('form');
-	const detailsElementsArray = $('details');
 	const advSearchButton = $('.adv-search-button');
 	const advSearchWrapper = $('.adv-search-wrapper');
-	const actionInputs = $('[name="p_agency_filterid"]');
-	console.log(actionInputs)
+
+	$(document).ready(function(){
+		$('.selectpicker').selectpicker();
+		$('.btn.dropdown-toggle.btn-default.bs-placeholder').hide();
+		$('.btn.dropdown-toggle.btn-default').hide();
+	})
 
 	// toggle advanced search function
 	advSearchButton.on('click', function (e) {
@@ -16,73 +19,4 @@ export function customFormInit() {
 		advSearchWrapper.slideToggle(300);
 	});
 
-	// 'checked' toggle listener for form action type (sale / rent)
-	actionInputs.on('click', function(e){
-		const clickedInput = e.target;
-		Array.from(actionInputs).forEach(input => input.removeAttribute('checked'))
-		clickedInput.setAttribute('checked', '')
-	})
-
-
-	// listener for custom select elements
-	if (detailsElementsArray.length > 0) {
-		Array.from(detailsElementsArray).forEach((targetDetail) => {
-			$(document).on('click tap touchstart', function (e) {
-				// listener for open or close
-				if (targetDetail && !targetDetail.contains(e.target)) {
-					targetDetail.removeAttribute('open');
-				}
-			});
-			// update <summary>
-			$(targetDetail).on('click tap touchstart', (e) => {
-				updateSummary(e.currentTarget);
-			});
-		});
-	}
-
-	function updateSummary(targetElement) {
-		const options = $('input[type=checkbox]');
-		console.log(options)
-
-		const selectedValues = Array.from(options)
-			.filter((option) => option.checked)
-			.map((option) => option.nextElementSibling)
-			.map((option) => option.setAttribute('checked' , ''))
-		const selectedValuesText = Array.from(options)
-			.filter((option) => option.checked)
-			.map((option) => option.nextElementSibling.textContent)
-			.join(', ');
-
-		if (selectedValues.length === 0) {
-			targetElement.querySelector('summary').textContent = 'Nothing selected';
-		} else if (selectedValues.length <= 2) {
-			targetElement.querySelector('summary').textContent = selectedValuesText;
-		} else {
-			targetElement.querySelector(
-				'summary'
-			).textContent = `Selected ${selectedValues.length} locations`;
-		}
-	}
-}
-
-export function setQueryParameter(){
-    const queryParameter = [];
-    const formProperties =form.querySelectorAll('[checked]');
-    console.log('formProperties', formProperties);
-    Array.from(formProperties).forEach(option=>{
-        // if option 'checked' and started from 'form-' - this is query parameter
-        if (option.hasAttribute('checked') && [...option.attributes].some(attr => attr.name.startsWith('form-'))){
-            console.log('option in checked and attribute:',option)
-            const formAttr = [...option.attributes].find(attr => attr.name.startsWith('form-'));
-            if (formAttr) {
-                // push query parameter name and value to array 'query parameter'
-                const formattedQuery = `{"${formAttr.name.substring(5)}":"${formAttr.value}"}`;
-                queryParameter.push(JSON.parse(formattedQuery));
-            }
-        }
-    })
-
-    console.log('queryParameter', queryParameter)
-
-    return queryParameter;
 }
